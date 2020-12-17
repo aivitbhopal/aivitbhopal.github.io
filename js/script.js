c6edc56603e1bb516a0eb14f11d8a21f59ef4e36
Kumar Prakhar <kumarprkhr@gmail.com>
2020-12-17 19:25:45 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Navbar
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
