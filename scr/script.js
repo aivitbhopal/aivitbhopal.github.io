f348833fd131b51e7c3d8e4e84ece28643c53ba7
Kumar Prakhar <kumarprkhr@gmail.com>
2021-02-11 11:34:38 +0530

    main

---
+++ scr/script.js
@@ -0,0 +1,25 @@
+const second = 1000,
+            minute = second * 60,
+            hour = minute * 60,
+            day = hour * 24;
+
+        let countDown = new Date('Feb 26, 2021 09:30:00').getTime(),
+            x = setInterval(function() {
+
+            let now = new Date().getTime(),
+                distance = countDown - now;
+
+            document.getElementById('days').innerText = Math.floor(distance / (day)),
+                document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+                document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+                document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+            
+            // do something later when date is reached
+            if (distance < 0) {
+             clearInterval(x);
+             "Conclave has begun";
+            }
+
+            }, second)
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
