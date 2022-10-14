44b3b3501fb11c350a223c5bbacd55c1da7b46e0
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2022-10-14 16:18:55 +0530

    fix large screen nav

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,12 @@
-function toggleMobileMenu(menu) {
-    menu.classList.toggle('open');
-}
+myID = document.getElementById("arbazbtn");
 
+var myScrollFunc = function () {
+    var y = window.scrollY;
+    if (y >= 500) {
+        myID.className = "showarbnav"
+    } else {
+        myID.className = "hidearbnav"
+    }
+};
+
+window.addEventListener("scroll", myScrollFunc);
