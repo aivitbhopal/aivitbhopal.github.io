14c42f2bc1603527fd6c8e7407d59cd056d64e73
SakkyOP <saksham298@gmail.com>
2023-06-21 19:41:50 +0530

    Revert "Revert "Revert "Revert "Merge branch 'main' into Development""""
    This reverts commit 79b78a9c707ed798965a62f0a9a494e1c29c0f6f.

---
+++ js/cart_animation.js
@@ -0,0 +1,46 @@
+
+// cartButton has been declare in cart_menu.js so no need to declare twice.
+// const cartButton = document.getElementById('cartButton');
+
+const overlay = document.getElementById('overlay');
+const checkoutContainer = document.getElementById('checkoutContainer');
+const exitButton = document.getElementById("exit-checkout");
+
+console.log(cartButton);
+
+var checkout = false; // State that checks if the checkout menu is open or not
+
+// Define the animation timeline
+const timeline = gsap.timeline({ paused: true });
+timeline
+  .set([overlay, checkoutContainer], { opacity: 0, pointerEvents: 'auto', display: "flex" })
+  .to(overlay, { opacity: 1, duration: 0.1 })
+  .to(checkoutContainer, { opacity: 1, duration: 0.1, scale: 1.1 })
+  .to(checkoutContainer, { scale: 0.9, duration: 0.1 })
+  .to(checkoutContainer, { scale: 1, duration: 0.1 })
+  .set([overlay, checkoutContainer], { pointerEvents: 'auto' });
+
+// Event listener for the cart button
+cartButton.addEventListener('click', () => {
+
+  // Reset the animation timeline
+  if (!checkout){
+    checkout = true;
+    timeline.restart();
+  }
+    
+});
+
+overlay.addEventListener('click', () => {
+    if (checkout) {
+        checkout = false;
+        timeline.reverse();
+    }
+});
+
+exitButton.addEventListener("click", () => {
+    if (checkout) {
+        checkout = false;
+        timeline.reverse();
+    }
+});
