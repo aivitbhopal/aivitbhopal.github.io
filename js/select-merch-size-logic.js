1793848ceb8b29958943b5970d883296c4db1260
SakkyOP <saksham298@gmail.com>
2024-01-20 13:57:59 +0530

    Added Data to the website

--- js/select-merch-size-logic.js
+++ js/select-merch-size-logic.js
@@ -1,31 +1,31 @@
 // keep a default selected value
-const size = document.getElementById("size"); // Size : S M L XL XXL
-const type = document.getElementById("type"); // Type : Normal Oversized
-const size_white = document.getElementById("size white"); // Size : S M L XL XXL
-const type_white = document.getElementById("type black"); // Type : Normal Oversized
+const size = '' // Size : S M L XL XXL
+const type = '' // Type : Normal Oversized
+const size_white = '' // Size : S M L XL XXL
+const type_white = '' // Type : Normal Oversized
 const link = document.getElementById("merch-black"); // Select the buy button element
 const link_white = document.getElementById("merch-white"); // Select the buy button element
 
 link_white.addEventListener("click", () => {
 	window.location.href = `https://docs.google.com/forms/d/e/1FAIpQLSdzX7zdGPz9ziH-fEB1PnjE7LTtBgY5RIrNFIvm06FxJgj81Q/viewform?usp=pp_url&entry.217624640=${
-		type.options[type.selectedIndex].value === "Normal"
+		type_white === "Normal"
 			? "Normal+Fit+White+t-shirt+(200+GSM)+INR+400"
 			: "Oversized+White+t-shirt+(220+GSM)+INR+500"
 	}&entry.${
-		type.options[type.selectedIndex].value === "Normal"
+		type_white === "Normal"
 			? "1021208672"
 			: "1445181987"
-	}=${size.options[size.selectedIndex].value}`;
+	}=${size_white}`;
 });
 
 link.addEventListener("click", () => {
 	window.location.href = `https://docs.google.com/forms/d/e/1FAIpQLSdpoME1rrPbhb7M6THUeYA5Wa6ywGs-TU73JGb8__5nqe4Wog/viewform?usp=pp_url&entry.557791790=${
-		type.options[type.selectedIndex].value === "Normal"
+		type === "Normal"
 			? "Normal+Fit+Black+t-shirt+(200+GSM)+INR+400"
 			: "Oversized+Black+t-shirt+(220+GSM)+INR+500"
 	}&entry.${
-		type.options[type.selectedIndex].value === "Normal"
+		type === "Normal"
 			? "1906753020"
 			: "1791548458"
-	}=${size.options[size.selectedIndex].value}`;
+	}=${size}`;
 });
